41de890e2e86fe8f21de35fa0406d0b9e4d66592
joseph <jlong6@unb.ca>
2019-01-11 23:43:03 -0400

    .

--- Lab1.java
+++ Lab1.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 class Lab1
 {
-    public static void main (String [] args)
+    public static void main (String [] args) //frick
     {
         Scanner scanTron = new Scanner(System.in);
         Project [] projectList = new Project [1]; 
